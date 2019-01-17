489d8623759cfa4c49f24650191fae7e7306bc9a
Slaymen <slaymen.hazzouri@gmail.com>
2019-01-17 17:05:42 +1100

    Update bot.js

--- bot.js
+++ bot.js
@@ -65,19 +65,14 @@ client.on("message", async message => {
     const m = await message.channel.send("***COMMANDS | [ .kick, .ban, .purge, .listvip, .upgrade, .pfp ]***");
   }
   if(command === "test") {
-    module.exports.run = async (bot, message, args) => {
-    let sicon = message.guild.iconURL;
-    let serverembed = new Discord.RichEmbed()
-    .setDescription("Server Information")
-    .setColor("#15f153")
-    .setThumbnail(sicon)
-    .addField("Server Name", message.guild.name)
-    .addField("Created On", message.guild.createdAt)
-    .addField("You Joined", message.member.joinedAt)
-    .addField("Total Members", message.guild.memberCount);
 
-    message.channel.send(serverembed);
-}
+    let botembed = new Discord.RichEmbed()
+    .setDescription("Slayz")
+    .setColor("#15f153")
+    .addField("Test")
+    
+    return message.channel.send(botembed);
+  }
 
 module.exports.help = {
   name:"serverinfo"
